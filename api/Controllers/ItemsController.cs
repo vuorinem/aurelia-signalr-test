6ab484e4b659348841c773a176e3458f4682dffa
Mikko Vuorinen <vuorinem@gmail.com>
2019-03-24 23:49:37 +0000

    Add CRUD controller for TODO items

---
+++ api/Controllers/ItemsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AureliaSignalRTest.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase {
+        private static List<Item> items = new List<Item>() {
+            new Item { Id = 1, Label = "Buy bananas", IsDone = true },
+            new Item { Id = 2, Label = "Call home", IsDone = false },
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Item>> Get() {
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> Get(int id) {
+            return items.Where(i => i.Id == id).SingleOrDefault();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string label) {
+            items.Add(new Item {
+                Id = items.Max(i => i.Id) + 1,
+                Label = label,
+                IsDone = false,
+            });
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Item update) {
+            var item = items.Where(i => i.Id == id).SingleOrDefault();
+
+            item.Label = update.Label;
+            item.IsDone = update.IsDone;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+            items.RemoveAll(i => i.Id == id);
+        }
+    }
+}
---
+++ api/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models {
+    public class Item {
+        public string Label { get; internal set; }
+        public bool IsDone { get; internal set; }
+        public int Id { get; internal set; }
+    }
+}
